d76469351e74fe2a7e2c6f7100dfdf7091634992
Alexandr Ovchinnikov <Alintorius@gmail.com>
2018-01-03 21:38:34 +0700

    add UIViewController storyboardInstance extension

---
+++ KeepAndWash/Extensions/UIViewController+StoryboardInstance.swift
@@ -0,0 +1,11 @@
+
+import UIKit
+extension UIViewController {
+    private class func storyboardInstancePrivate<T: UIViewController>() -> T? {
+        let storyboard = UIStoryboard(name: String(describing: self), bundle: nil)
+        return storyboard.instantiateInitialViewController() as? T
+    }
+    class func storyboardInstance() -> Self? {
+        return storyboardInstancePrivate()
+    }
+}
